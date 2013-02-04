4d108e1eb829bcac84d82c9bdc58387126a7df77
Gergely Imreh <imrehg@gmail.com>
2013-02-04 10:45:14 +0800

    working version with detailed output

--- banking.sh
+++ banking.sh
@@ -7,50 +7,60 @@ URL3=https://www.mybank.com.tw/pweb/main_menu.asp
 URL4=https://www.mybank.com.tw/pweb/main_menu_bottom.asp
 URL5=https://www.mybank.com.tw/pweb/C01.asp
 
+OUT1=url1.html
+OUT2=url2.html
+OUT3=url3.html
+OUT4=url4.html
+
+if [ -f cookies.txt ]; then
+  rm cookies.txt
+fi
+
 USERAGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17"
 
 # main page to get cookie
 curl \
-     --silent \
+     -ss \
      --location \
      --user-agent "${USERAGENT}" \
      -b cookies.txt \
      -c cookies.txt \
-     ${URL1}
+     ${URL1} > ${OUT1}
 
 # logging in
 curl \
-     --silent \
+     -ss \
      --location \
      --user-agent "${USERAGENT}" \
      -b cookies.txt \
      -c cookies.txt \
      --referer ${URL1} \
      --data "logon_name=${ID}&logon_pwd=${PIN}&nick_name=${CODE}&ChipCard=N&ComeFrom=MyBank&Fun=&wid=&wp_kind=&FundID=&FundType=&ClickFlag=&capslock_status=&kbdid=" \
-     ${URL2}
+     ${URL2} > ${OUT2}
 
 # menu page
 curl \
-     --silent \
+     -ss \
      --location \
      --user-agent "${USERAGENT}" \
      -b cookies.txt \
      -c cookies.txt \
      --referer ${URL2} \
-     ${URL3}
+     ${URL3} > ${OUT3}
 
 # menu side
 curl \
-     --silent
+     -ss \
      --location \
      --user-agent "${USERAGENT}" \
      -b cookies.txt \
      -c cookies.txt \
      --referer ${URL3} \
-     ${URL4}
+     ${URL4} > ${OUT4}
 
 # actual data
 curl \
+     -ss \
      --location \
      --user-agent "${USERAGENT}" \
      -b cookies.txt \
